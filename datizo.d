24671e5edb12201c7df88425318078941c86a6fd
Redger I. Cornice <idriss.cornice@gmail.com>
2015-01-28 23:41:29 +0100

    definition of aliases

---
+++ datizo.d
@@ -0,0 +1,16 @@
+module datizo;
+
+alias ubyte  uint08;
+alias ushort uint16;
+alias uint   uint32;
+alias ulong  uint64;
+
+alias byte   int08;
+alias short  int16;
+alias int    int32;
+alias long   int64;
+
+void main() {
+
+
+}
